33b8321ebaade104c9904202297b9758527f43c7
Shawn Lee <changoloco.evg@gmail.com>
2022-05-29 19:50:50 -0500

    :technologist: chore(alias): add assets path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import windicss from "vite-plugin-windicss";
 import solid from "vite-plugin-solid";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), windicss()],
+  resolve: {
+    alias: {
+      "@assets": resolve("src/assets"),
+      "@assets/*": resolve("src/assets/*")
+    }
+  },
   build: {
     target: "esnext",
     polyfillDynamicImport: false
